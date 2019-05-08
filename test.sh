e3ec57dde18d8f1ede92cf846ed4a160bf04bf5c
Rafia Sabih <rafia.sabih@zalando.de>
2019-05-08 11:07:17 +0200

    Adding travis file

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+
+version=$(postgres -V | sed -n 's/^.* \([1-9][0-9]*\(\.[0-9]*\)\{0,2\}\).*/\1/p')
+version=${version%.*}
+
+rm -fr test_cluster*
+set -e
+
+readonly port=5440
+
+function start_postgres() {
+    postgres -D test_cluster$1 --port=$(($port+$1)) &
+    max_attempts=0
+    while ! pg_isready -h localhost -p $(($port+$1)) -d postgres; do
+        [[ $((max_attempts++)) -lt 10 ]] && sleep 1 || exit 1
+    done
+}
+ 
+function shutdown_clusters() {
+    set +e
+    pg_ctl -w -D test_cluster0 stop -mf
+    pg_ctl -w -D test_cluster1 stop -mf
+}
+
+function create_cluster() {
+    initdb test_cluster$1
+    echo "host all all 	all" >> test_cluster$1/pg_hba.conf
+    echo "shared_preload_libraries = 'pg_auth_mon'" >> test_cluster$1/postgresql.conf
+
+    start_postgres $1
+}
+
+create_cluster 0
+
+PGPASSWORD=postgres psql -U none -d postgres
+PGPASSWORD=postgres psql -U postgres -d postgres -c "CREATE EXTENSION pg_auth_mon"
+PGPASSWORD=postgres psql -U postgres -d postgres -c "SELECT uid, successful_attempts, total_hba_conflicts, other_auth_failures FROM pg_auth_mon()"
+
+shutdown_clusters
