b74e31e64ef163cc892edac97f315b4fa99cfb68
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2019-08-05 12:28:17 +0200

    Missing file

--- sql/pg_auth_mon.sql
+++ sql/pg_auth_mon.sql
@@ -7,19 +7,19 @@ create database testdb;
 create extension pg_auth_mon;
 
 --1.Successful Login attempt
-\! PGPASSWORD=postgres psql -U auth_super -d testdb -c "select 1"
+\! PGPASSWORD=postgres psql -X -U auth_super -d testdb -c "select 1"
 select rolname, successful_attempts, total_hba_conflicts, other_auth_failures from pg_auth_mon where rolname like 'auth_%';
 
 --2.Login attempt by invalid username
-\! PGPASSWORD=postgres psql -U auth_blah -d testdb  -c "select 1"
+\! PGPASSWORD=postgres psql -X -U auth_blah -d testdb  -c "select 1" 2>&1 | sed 's/^.* FATAL: */FATAL: /'
 select rolname, uid, successful_attempts, total_hba_conflicts, other_auth_failures from pg_auth_mon where uid = 0;
 
 --3.Login attempt by username who is not allowed to login is not authentication failure
-\! PGPASSWORD=postgres psql -U auth_nologin -d testdb -c "select 1"
+\! PGPASSWORD=postgres psql -X -U auth_nologin -d testdb -c "select 1" 2>&1 | sed 's/^.* FATAL: */FATAL: /'
 select rolname, successful_attempts, total_hba_conflicts, other_auth_failures from pg_auth_mon where rolname like 'auth_%';
 
 --4.Login attempt by a valid user with a wrong password 
-\! PGPASSWORD=postgres psql -U auth_test -d testdb -c "select 1"
+\! PGPASSWORD=postgres psql -X -U auth_test -d testdb -c "select 1" 2>&1 | sed 's/^.* FATAL: */FATAL: /'
 select rolname, successful_attempts, total_hba_conflicts, other_auth_failures from pg_auth_mon where rolname like 'auth_%' order by rolname;
 
 --Cleanup
